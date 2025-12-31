a9dc71687b3a7563b0ba3b4812bb457434eaa9b6
yy923683900 <923683900@qq.com>
2025-12-31 15:03:32 +0800

    修复featureid报错

--- src/utils/FeatureIdUniforms.ts
+++ src/utils/FeatureIdUniforms.ts
@@ -12,12 +12,19 @@ export class FeatureIdUniforms {
 
   get value() {
     const idMap = this.mesh.userData.idMap;
-    const mappedValues = this.plugin.oids.map((oid) => idMap[oid]);
+    
+    // 如果 idMap 不存在，返回全部填充为 -1 的数组
+    if (!idMap) {
+      return new Array(this.plugin.getFeatureIdCount()).fill(-1);
+    }
     
     // 填充剩余位置为-1，避免与featureId为0的mesh冲突
     const result = new Array(this.plugin.getFeatureIdCount()).fill(-1);
-    for (let i = 0; i < mappedValues.length; i++) {
-      result[i] = mappedValues[i];
+    for (let i = 0; i < this.plugin.oids.length; i++) {
+      const oid = this.plugin.oids[i];
+      const featureId = idMap[oid];
+      // 如果找到对应的 featureId，则使用它；否则保持 -1
+      result[i] = featureId !== undefined ? featureId : -1;
     }
     
     return result;
